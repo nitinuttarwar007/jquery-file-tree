d4182b96b0a4ccdae66004de162788c1fa3be799
Vivek Kumar Bansal <vkbdigitaldesigns@gmail.com>
2014-07-29 10:43:23 +0530

    Refactored Tests

--- test/spec/spec.js
+++ test/spec/spec.js
@@ -7,9 +7,12 @@ function setupUlFixture(){
 }
 
 
-var data = [{"name":"A","type":"folder","children":[{"name":"001","ext":"txt","type":"file"},{"name":"002","ext":"txt","type":"file"},{"name":"AA","type":"folder","children":[{"name":"003","ext":"txt","type":"file"},{"name":"004","ext":"txt","type":"file"}]}]},{"name":"B","type":"folder","children":[{"name":"01","ext":"jpg","type":"file"},{"name":"02","ext":"jpg","type":"file"},{"name":"B","type":"folder","children":[]}]}];
+var data = [{"name":"A","type":"folder","children":[{"name":"001","ext":"txt","type":"file"},{"name":"002","ext":"txt","type":"file"},{"name":"AA","type":"folder","children":[{"name":"003","ext":"txt","type":"file"},{"name":"004","ext":"txt","type":"file"}]}]},{"name":"B","type":"folder","children":[{"name":"01","ext":"jpg","type":"file"},{"name":"02","ext":"jpg","type":"file"},{"name":"B","type":"folder","children":[]}]}],
+    button;
 
 function init(data, options){
+    $.fx.off = true;
+
     return $("#test").filetree({
         data: data
     });
@@ -21,90 +24,56 @@ describe("filetree", function() {
 
         beforeEach(function(){
             setupEmptyFixture();
-            init(data).on('click.folder.filetree', function(ev){
-                $(this).filetree('toggle', ev.target);
-            }).on('closed.folder.filetree', function(ev){
-                console.log('closed');
-            });
-            jasmine.clock().install();
+            init(data);
+            button = $('li.folder').eq(0).find('button');
+            
+
         });
 
         afterEach(function() {
-            jasmine.clock().uninstall();
+            
         });
 
-        it("should trigger click.folder.filetree on click", function() {
+
+        it("should trigger click events", function() {
             
-            var spy = spyOnEvent('li.folder:first a', 'click.folder.filetree');
+            var spy_folder = spyOnEvent('li.folder:first > a', 'click.folder.filetree');
+            var spy_file = spyOnEvent('li.file:first > a', 'click.file.filetree');
+
+            $('li.folder').eq(0).find('a').trigger('click');
             
-            $('li.folder a').eq(0).trigger('click');
+            expect('click.folder.filetree').toHaveBeenTriggeredOn('li.folder:first > a');
+            expect(spy_folder).toHaveBeenTriggered();
+
+            $('li.folder').eq(0).find('a').trigger('click');
             
-            expect('click.folder.filetree').toHaveBeenTriggeredOn('li.folder:first a');
-            expect(spy).toHaveBeenTriggered();
+            expect('click.file.filetree').toHaveBeenTriggeredOn('li.file:first > a');
+            expect(spy_file).toHaveBeenTriggered();
         });
 
-        it("should trigger click.file.filetree on click", function() {
-            
-            var spy = spyOnEvent('li.file:first a', 'click.file.filetree');
-            
-            $('li.file a').eq(0).trigger('click');
-            
-            expect('click.file.filetree').toHaveBeenTriggeredOn('li.file:first a');
-            expect(spy).toHaveBeenTriggered();
-        });
 
-        it("should trigger open.folder.filetree before opening folder", function() {
+        it("should trigger open/close events", function() {
             
-            var spy = spyOnEvent('li.folder:first a', 'open.folder.filetree');
-            
-            $('li.folder a').eq(0).trigger('click');
-            
-            expect('open.folder.filetree').toHaveBeenTriggeredOn('li.folder:first a');
-            expect(spy).toHaveBeenTriggered();
-        });
+            var spy_open = spyOnEvent('li.folder:first > a', 'open.folder.filetree');
+            var spy_opened = spyOnEvent('li.folder:first > a', 'opened.folder.filetree');
+            var spy_close = spyOnEvent('li.folder:first > a', 'close.folder.filetree');
+            var spy_closed = spyOnEvent('li.folder:first > a', 'closed.folder.filetree');
 
-        it("should trigger opened.folder.filetree after folder has opened", function() {
-            
-            var spy = spyOnEvent('li.folder:first a', 'opened.folder.filetree');
-            
-            $('li.folder a').eq(0).trigger('click');
-            jasmine.clock().tick(400);
-            expect('opened.folder.filetree').toHaveBeenTriggeredOn('li.folder:first a');
-            expect(spy).toHaveBeenTriggered();
-        });
+            button.trigger('click');
 
-        it("should trigger close.folder.filetree before closing folder", function() {
+            expect('open.folder.filetree').toHaveBeenTriggeredOn('li.folder:first > a');
+            expect(spy_open).toHaveBeenTriggered();
             
-            var spy = spyOnEvent('li.folder:first a', 'close.folder.filetree');
-            
-            $('li.folder a').eq(0).trigger('click');
+            expect('opened.folder.filetree').toHaveBeenTriggeredOn('li.folder:first > a');
+            expect(spy_opened).toHaveBeenTriggered();
+    
+            button.trigger('click');
 
-            setTimeout(function(){ 
-                $('li.folder a').eq(0).trigger('click'); 
-            }, 400)
-            
-            jasmine.clock().tick(400);
+            expect('close.folder.filetree').toHaveBeenTriggeredOn('li.folder:first > a');
+            expect(spy_close).toHaveBeenTriggered();
 
-            expect('close.folder.filetree').toHaveBeenTriggeredOn('li.folder:first a');
-            expect(spy).toHaveBeenTriggered();
-        });
-
-        it("should trigger closed.folder.filetree after folder has closed", function() {
-            
-            var spy = spyOnEvent('li.folder:first a', 'closed.folder.filetree');
-            
-            $('li.folder a').eq(0).trigger('click');
-            
-            setTimeout(function(){ 
-                $('li.folder a').eq(0).trigger('click'); 
-            }, 500);
-            
-            jasmine.clock().tick(400);
-            jasmine.clock().tick(100);
-            jasmine.clock().tick(401);
-
-            expect('closed.folder.filetree').toHaveBeenTriggeredOn('li.folder:first a');
-            expect(spy).toHaveBeenTriggered();
+            expect('closed.folder.filetree').toHaveBeenTriggeredOn('li.folder:first > a');
+            expect(spy_closed).toHaveBeenTriggered();
         });
         
     });
