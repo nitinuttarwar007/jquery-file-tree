09bddc839d45e9add908aec719d4601d1ee1a2ae
Vivek Kumar Bansal <vkb0310@gmail.com>
2014-12-03 12:19:53 +0530

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
+    bower = require('./bower.json')
     pack = require('path').join(process.cwd(), 'package.json'),
+    minimist = require('minimist'),
     $ = require('gulp-load-plugins')({
         pattern : 'gulp-*',
         config: pack,
@@ -10,8 +12,19 @@ var gulp = require('gulp'),
         
     });
 
+var knownOptions = {
+  boolean: ['patch', 'major', 'minor'],
+  default: {
+    patch: true
+  }
+};
+
+var options = minimist(process.argv.slice(2), knownOptions);
+
+
 gulp.task('coffee', function () {
     return gulp.src('./src/coffee/*.coffee')
+        .pipe($.tokenReplace({global:bower}))
         .pipe($.coffeelint())
         .pipe($.coffeelint.reporter())
         .pipe($.coffee({bare : true })).on('error', $.util.log)
@@ -42,4 +55,12 @@ gulp.task('watch', ['coffee','less'], function(){
     gulp.watch('./src/coffee/*.coffee', ['coffee']);
     gulp.watch('./src/less/*.less', ['less']);
     gulp.watch('./test/spec/*.js').on('change', $.livereload.changed);
+});
+
+gulp.task('bump', function(){
+    return gulp.src(['./bower.json', './package.json'])
+        .pipe($.if((options.patch && !options.major && !options.minor), $.bump()))
+        .pipe($.if(options.minor, $.bump({type: 'minor'})))
+        .pipe($.if(options.major, $.bump({type: 'major'})))
+        .pipe(gulp.dest('./'));
 });
